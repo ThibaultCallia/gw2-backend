1d5d322e9201a0545562d75c72183bc8db4af028
Thibault Calliauw <calliauw.t@gmail.com>
2023-03-21 10:22:55 +0100

    CORS issues solved

--- src/api.php
+++ src/api.php
@@ -6,6 +6,10 @@ $response = new StdClass;
 require_once "api/lists.inc.php";
 require_once "api/products.inc.php";
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
 
 header('Content-Type: application/json; charset=utf-8');
 print json_encode($response);
